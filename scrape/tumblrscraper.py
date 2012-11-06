80ca787395e0e4f3fcae54a04fd55ce51d97f536
Matthew Wampler-Doty <matt@w-d.org>
2012-11-06 07:13:11 -0600

    - Ignoring .db files - Some fixes to scraper - Using Nor's hacked tumblpy - Moving tests to test directory

--- scrape/tumblrscraper.py
+++ scrape/tumblrscraper.py
@@ -5,7 +5,7 @@ import sqlite3
 import ts_model
 import sys
 
-def tumblr_scraper(base_url,db_name,num_images,start_offset=0,limit=20,url_type='blog'):
+def tumblr_scraper(base_url,db_name,num_images,start_offset=0,limit=20,url_type='blog',tag=None):
     #init with some key
     t = tumblpy.Tumblpy(app_key = 'V55FKUe1lMSdx0UyGSFknmO8DoSaeNzT9oByUwOE1Hvp7diQJ7',
                         app_secret = 'TD9eTgRhoo8ceu0cjcF0nROWAAMkst1uAkSx5XuSOjnYxrGq50',
@@ -28,11 +28,19 @@ def tumblr_scraper(base_url,db_name,num_images,start_offset=0,limit=20,url_type=
     i = 0
     while n < num_images :
         #get the posts
-        print "Get posts %i to %i" % (i*limit+start_offset,(1+i)*limit+start_offset)
         if url_type == 'blog':
-           posts = t.get('posts',blog_url=base_url,params={'limit':limit, 'offset':i*limit+start_offset})
+            print "Get posts %i to %i" % (i*limit+start_offset,(1+i)*limit+start_offset)
+            posts = t.get('posts',blog_url=base_url,extra_endpoints='photo', params={'limit':limit, 'offset':i*limit+start_offset})
+            posts = posts['posts']
+        if url_type == 'tag':
+            print "Get posts %i posts before timestamp %i" % (limit,i)
+            if i == 0:
+                params = {'limit':limit, 'tag':tag};
+            else:
+                params = {'limit':limit, 'tag':tag, 'before':i};
+            posts = t.get(None,blog_url=None,tag=True, params=params)
         i += 1
-        for p in posts['posts']:
+        for p in posts:
           #some posts don't have photo
           if(not('photos' in  p)): continue
           #some posts have more than one image, we will ignore that for now
@@ -41,26 +49,40 @@ def tumblr_scraper(base_url,db_name,num_images,start_offset=0,limit=20,url_type=
           if(len(p['tags']) == 0): continue
           # If we made it through that, we have a new photo
           n += 1
+          #if we're getting from tag we need timestamp
+          if url_type == 'tag':
+            i = p['timestamp']
           #print out the info, move to DB later
-          note_count = p['note_count']
           tags = [ y.strip().lower() for x in p['tags']
                                      for y in x.split('\n') ]
           url = p['photos'][0]['original_size']['url']
           #if this is slow, switch to batch execute instead
-          print "Found %s %i: %s %s" % (sys.argv[1],n,url,"#" + " #".join(tags))
+          if 'note_count' in p.keys():
+              note_count = p['note_count']
+          else:
+              note_count = -1
+          if tag : print "Found %s %i (notes=%i): %s %s" % (tag, n, note_count, url,
+                                                            "#" + " #".join(tags))
           ts_model.add_tags(c, tags)
           ts_model.add_photo(c, url, note_count)
           ts_model.link_tags_photo(c, tags, url)
           conn.commit()
     conn.close()
 
-if __name__ == "__main__" : 
-    #subject to cmd input
-    base_url='http://%s.tumblr.com' % sys.argv[1]
-    db_name = '%s.db' % sys.argv[1]
-    num_images = int(sys.argv[2])
-    if len(sys.argv) >= 4:
-       start_offset = int(sys.argv[3])
+if __name__ == "__main__" :
+    base_url=None
+    tag=None
+    if sys.argv[1] == 'blog':
+        base_url='http://%s.tumblr.com' % sys.argv[2]
+    else:
+        tag = sys.argv[2]
+    db_name = '%s.db' % sys.argv[2]
+    num_images = int(sys.argv[3])
+    if len(sys.argv) >= 5:
+       start_offset = int(sys.argv[4])
     else :
        start_offset = 0
-    tumblr_scraper(base_url,db_name,num_images,start_offset=start_offset,limit=20)
+    if tag:
+        tumblr_scraper(None, db_name, num_images, start_offset=start_offset, limit=20, url_type='tag',tag=tag)
+    else:
+        tumblr_scraper(base_url,db_name,num_images,start_offset=start_offset,limit=20)
